59d16a8767f9796df5cd4d7a683cc3adcf377b80
johndward01 <johndward01@gmail.com>
2023-04-25 14:26:16 -0500

    init

---
+++ Aaron_Romero_MVC_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Aaron_Romero_MVC_Demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
